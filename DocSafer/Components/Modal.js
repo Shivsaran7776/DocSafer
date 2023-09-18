f8a183e0fa69bd0423aa334ce4203a47309cc5f5
AshwinRosario <ashwinkaranthamalai@gmail.com>
2023-09-18 11:15:04 +0530

    upload page

---
+++ DocSafer/Components/Modal.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Modal, View, Text, Button, StyleSheet } from 'react-native';
+
+const MyModal = ({ visible, onDismiss, onSelect }) => {
+  return (
+    <View animationType="slide" transparent={true} visible={visible}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalText}>Select a PDF document</Text>
+          <Button title="Pick a PDF" onPress={onSelect} />
+          <Button title="Cancel" onPress={onDismiss} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
+
+export default MyModal;
--- DocSafer/Components/UploadFile.js
+++ DocSafer/Components/UploadFile.js
@@ -1,41 +1,85 @@
-import React from 'react';
-import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, SafeAreaView, Modal, Button } from 'react-native';
 import Background from './Background';
 import Btn from './Btn';
 import { darkGreen, green } from './Constants';
 import { StatusBar } from 'expo-status-bar';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
+import * as DocumentPicker from 'expo-document-picker';
+import MyModal from './Modal'; 
 
-const UploadFile=(props)=> {
-  
-  const handleSubmit = ()=>{
-   
-  }
+const UploadFile = (props) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleFilePick = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({ type: 'application/pdf' });
+      if (result.type === 'success') {
+        setSelectedFile(result);
+        setModalVisible(false);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSubmit = () => {
+    if (selectedFile) {
+      console.log('Selected File:', selectedFile);
+      // You can now send the selectedFile to your backend for storage in MongoDB using Axios or other methods.
+    } else {
+      console.log('No file selected.');
+    }
+  };
+
   return (
     <Background>
-      <SafeAreaView >
-        <StatusBar/>
+      <SafeAreaView>
+        <StatusBar />
         <View style={styles.container}>
-           <Text>UploadFile</Text>
-        </View>
-        <Btn
-        textColor="white"
-        bgColor={darkGreen}
-        btnLabel="Upload"
-        Press={handleSubmit}
-      />
-    </SafeAreaView>
+          <Btn
+            textColor="white"
+            bgColor={darkGreen}
+            btnLabel="Select PDF"
+            onPress={() => setModalVisible(true)}
+          />
+          <Btn margin textColor="white" bgColor={green} btnLabel="Upload" onPress={handleSubmit} />
+          </View>
+          <MyModal
+          visible={modalVisible}
+          onDismiss={() => setModalVisible(false)}
+          onSelect={handleFilePick}
+        />
+      </SafeAreaView>
     </Background>
-  )
-}
+  );
+};
 
-export default UploadFile
+export default UploadFile;
 
 const styles = StyleSheet.create({
-    container:{
-        top:320,
-        alignItems:'center',
-        backgroundColor:'white',
-    },
-})
+  container: {
+    alignItems: 'center',
+    alignContent:'center',
+    marginTop: 300,
+    marginLeft:25 ,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
